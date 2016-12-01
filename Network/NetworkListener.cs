883bea928af57643c8472c00fc820ba4ca727967
JoshBakker <joshuabakkerdev@outlook.com>
2016-12-01 11:33:24 +0100

    Added NetworkListener class.

---
+++ Network/NetworkListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleWebserver
+{
+	public class NetworkListener
+	{
+		private Socket _server;
+
+		public NetworkListener ()
+		{
+			
+		}
+	}
+}
+
